9d34954cdeb642be9c626bd2c575e4d04df2167d
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-05-15 01:11:25 +0300

    add db embeddings

--- src/service/api.py
+++ src/service/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from facenet.model import FaceNetModel
 import numpy as np
+from service.face_comparator import load_embeddings_from_database,compare_to_database
 from utils.gpu_utils import check_gpu_availability, configure_gpu   
 import os
 import cv2
@@ -13,6 +14,7 @@ if check_gpu_availability():
 # Load the FaceNet model
 model = FaceNetModel()
 model.load_model()
+database_embbeddings = load_embeddings_from_database(model)
 
 @app.route('/compare', methods=['POST'])
 def compare_faces():
@@ -23,7 +25,7 @@ def compare_faces():
     embedding = model.compute_embedding(photo)
 
     # Compare the embedding to the faces in the database
-    match = compare_to_database(embedding)
+    match = compare_to_database(embedding, database_embbeddings)
 
     # Return the result as JSON
     return jsonify({'match': match})
