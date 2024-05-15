37417916345d72028f4e198fc132429fe370e011
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-05-15 23:24:33 +0000

    update requirements

---
+++ test.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'http://localhost:5000/compare'
+file_path = 'src/db/test_faces/1.jpg'
+
+with open(file_path, 'rb') as f:
+    files = {'photo': f}
+    response = requests.post(url, files=files)
+
+print(response.json())
